e819d1fa9a56feab4bc79dc2ffa223f476083add
Naveen <Naveen@DESKTOP-ME48G5A>
2024-03-05 15:44:39 +0530

    It is my first class

---
+++ varshacorejava/src/corejava/sample.java
@@ -0,0 +1,15 @@
+package corejava;
+import java.util.*;
+public class sample {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("enter a and b values");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+        System.out.println("addition of a="+a+" and b="+b+"is:"+c);
+	}
+
+}
